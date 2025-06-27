548d3d3c17f97d5e8551b0b94253bd8561e58f7b
shreyashdhas0604 <shreyashrdhas@gmail.com>
2025-06-28 03:25:18 +0530

    video-processing-service running

---
+++ video-processing-service/src/main/java/com/example/video_processing_service/VideoProcessingServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.video_processing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoProcessingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VideoProcessingServiceApplication.class, args);
+    }
+}
